63ed622ea9d40e96cff3625b5c98dee6e2104ca4
Tikout <131374666+Tikout@users.noreply.github.com>
2023-08-21 04:12:57 +0200

    Delete dist directory

--- dist/auth/users.repository.d.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from 'typeorm';
-import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { User } from './user.entity';
-export declare class UsersRepository extends Repository<User> {
-    private logger;
-    createUser(authCredentialsDto: AuthCredentialsDto): Promise<void>;
-}
